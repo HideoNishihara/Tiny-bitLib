fc449e8111d7ef779d11604753abbf4e684459f4
HP-PC\hideo <hideo.n@jcom.home.ne.jp>
2025-01-13 11:53:47 +0900

    Initial commit8

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # Tiny:bit
 
-Extension for Yahboom Tiny:bit V2.0.5
+Extension for Yahboom Tiny:bit V2.0.3
 
 ## License
 
--- main.ts
+++ main.ts
@@ -15,7 +15,6 @@ namespace Tinybit {
     let yahStrip: neopixel.Strip;
 
     export enum enColor {
-
         //% blockId="OFF" block="OFF"
         OFF = 0,
         //% blockId="Red" block="Red"
@@ -32,10 +31,9 @@ namespace Tinybit {
         Pinkish,
         //% blockId="Yellow" block="Yellow"
         Yellow,
-
     }
-    export enum enMusic {
 
+    export enum enMusic {
         //% blockId="dadadum" block="dadadum"
         dadadum = 0,
         //% blockId="entertainer" block="entertainer"
@@ -76,10 +74,9 @@ namespace Tinybit {
         power_up,
         //% blockId="power_down" block="power_down"
         power_down
-
     }
-    export enum enPos {
 
+    export enum enPos {
         //% blockId="LeftState" block="LeftState"
         LeftState = 0,
         //% blockId="RightState" block="RightState"
@@ -92,8 +89,7 @@ namespace Tinybit {
         //% blockId="Black" block="Black Line"
         Black = 1
     }
-    
-    
+
     export enum CarState {
         //% blockId="Car_Run" block="Run"
         Car_Run = 1,
@@ -112,26 +108,23 @@ namespace Tinybit {
     }
 
     function setPwmRGB(red: number, green: number, blue: number): void {
-
         let buf = pins.createBuffer(4);
         buf[0] = RGB;
         buf[1] = red;
         buf[2] = green;
         buf[3] = blue;
-        
         pins.i2cWriteBuffer(PWM_ADD, buf);
     }
 
-    let car_flag_old = 0; //0：两电机正转 1：两电机反转 2:左旋 3：右旋
-    let car_flag_new = 0; //0：两电机正转 1：两电机反转 2:左旋 3：右旋
+    let car_flag_old = 0;		//0:両方のモーターが正転 1:両方のモーターが逆転 2:左回転 3:右回転
+    let car_flag_new = 0;		//0:両方のモーターが正転 1:両方のモーターが逆転 2:左回転 3:右回転
     function setPwmMotor(mode: number, speed1: number, speed2: number): void {
-        if (mode < 0 || mode > 6)
-            return;
-        
+        if (mode < 0 || mode > 6) return;
+
         let buf = pins.createBuffer(5);
         buf[0] = MOTOR;
         switch (mode) { 
-            case 0: buf[1] = 0; buf[2] = 0; buf[3] = 0; buf[4] = 0; break;              //stop
+            case 0: buf[1] = 0; buf[2] = 0; buf[3] = 0; buf[4] = 0; break;                                //stop
             case 1: buf[1] = speed1; buf[2] = 0; buf[3] = speed2; buf[4] = 0; car_flag_new = 0; break;    //run
             case 2: buf[1] = 0; buf[2] = speed1; buf[3] = 0; buf[4] = speed2; car_flag_new = 1; break;    //back
             case 3: buf[1] = 0; buf[2] = 0; buf[3] = speed2; buf[4] = 0; car_flag_new = 0;      break;    //left
@@ -139,12 +132,12 @@ namespace Tinybit {
             case 5: buf[1] = 0; buf[2] = speed1; buf[3] = speed2; buf[4] = 0; car_flag_new = 2; break;    //tleft
             case 6: buf[1] = speed1; buf[2] = 0; buf[3] = 0; buf[4] = speed2; car_flag_new = 3; break;    //tright
         }
-        if(car_flag_new != car_flag_old) //上一次状态是正转，这次是反转
+        if(car_flag_new != car_flag_old)			//前回は前進でしたが、今回は後退です
         {
             let bufff = pins.createBuffer(5);
             bufff[0] = MOTOR;
             bufff[1] = 0; bufff[2] = 0; bufff[3] = 0; bufff[4] = 0;
-            pins.i2cWriteBuffer(PWM_ADD, buf);//停止100ms
+            pins.i2cWriteBuffer(PWM_ADD, buf);		//停止100ms
             basic.pause(100);
             car_flag_old = car_flag_new;
         }
@@ -152,65 +145,61 @@ namespace Tinybit {
     }
 
     function Car_run(speed1: number, speed2: number) {
-
-
         setPwmMotor(1, speed1, speed2);
     }
 
     function Car_back(speed1: number, speed2: number) {
-
         setPwmMotor(2, speed1, speed2);
     }
 
     function Car_left(speed1: number, speed2: number) {
-
         setPwmMotor(3, speed1, speed2);
     }
 
     function Car_right(speed1: number, speed2: number) {
-
         setPwmMotor(4, speed1, speed2);
     }
 
     function Car_stop() {
-       
         setPwmMotor(0, 0, 0);
     }
 
     function Car_spinleft(speed1: number, speed2: number) {
-
         setPwmMotor(5, speed1, speed2);
-    } 
+    }
 
     function Car_spinright(speed1: number, speed2: number) {
-
         setPwmMotor(6, speed1, speed2);
     }
 
     /**
      * *****************************************************************
      * @param index
-     */   
+     */
 
+	//===========================================================
+	//	斜体ライトの状態（Neopixel指定）
+	//===========================================================
     //% blockId=Tinybit_RGB_Car_Program block="RGB_Car_Program"
     //% weight=99
     //% blockGap=10
     //% color="#006400"
     //% name.fieldEditor="gridpicker" name.fieldOptions.columns=4
     export function RGB_Car_Program(): neopixel.Strip {
-         
         if (!yahStrip) {
             yahStrip = neopixel.create(DigitalPin.P12, 2, NeoPixelMode.RGB);
         }
-        return yahStrip;  
-    }  
+        return yahStrip;
+    }
 
+	//===========================================================
+	//	ヘッドライト（基本色指定）
+	//===========================================================
     //% blockId=Tinybit_RGB_Car_Big block="RGB_Car_Big|value %value"
     //% weight=98
     //% blockGap=10
     //% name.fieldEditor="gridpicker" name.fieldOptions.columns=4
     export function RGB_Car_Big(value: enColor): void {
-
         switch (value) {
             case enColor.OFF: {
                 setPwmRGB(0, 0, 0);
@@ -246,16 +235,22 @@ namespace Tinybit {
             }
         }
     }
+
+	//===========================================================
+	//	ヘッドライト（ＲＧＢ指定）
+	//===========================================================
     //% blockId=Tinybit_RGB_Car_Big2 block="RGB_Car_Big2|value1 %value1|value2 %value2|value3 %value3"
     //% weight=97
     //% blockGap=10
     //% value1.min=0 value1.max=255 value2.min=0 value2.max=255 value3.min=0 value3.max=255
     //% name.fieldEditor="gridpicker" name.fieldOptions.columns=4
     export function RGB_Car_Big2(value1: number, value2: number, value3: number): void {
-
         setPwmRGB(value1, value2, value3);
-
     }
+
+	//===========================================================
+	//	メロディーを鳴らす
+	//===========================================================
     //% blockId=Tinybit_Music_Car block="Music_Car|%index"
     //% weight=95
     //% blockGap=10
@@ -285,9 +280,10 @@ namespace Tinybit {
             case enMusic.power_down: music.beginMelody(music.builtInMelody(Melodies.PowerDown), MelodyOptions.Once); break;
         }
     }
-    
-    
-    
+
+	//===========================================================
+	//	走行モード１
+	//===========================================================
     //% blockId=Tinybit_CarCtrl block="CarCtrl|%index"
     //% weight=93
     //% blockGap=10
@@ -304,7 +300,10 @@ namespace Tinybit {
             case CarState.Car_SpinRight: Car_spinright(255, 255); break;
         }
     }
-    
+
+	//===========================================================
+	//	走行モード２
+	//===========================================================
     //% blockId=Tinybit_CarCtrlSpeed block="CarCtrlSpeed|%index|speed %speed"
     //% weight=92
     //% blockGap=10
@@ -322,7 +321,10 @@ namespace Tinybit {
             case CarState.Car_SpinRight: Car_spinright(speed, speed); break;
         }
     }
-    
+
+	//===========================================================
+	//	走行モード３
+	//===========================================================
     //% blockId=Tinybit_CarCtrlSpeed2 block="CarCtrlSpeed2|%index|speed1 %speed1|speed2 %speed2"
     //% weight=91
     //% blockGap=10
@@ -339,34 +341,100 @@ namespace Tinybit {
             case CarState.Car_SpinLeft: Car_spinleft(speed1, speed2); break;
             case CarState.Car_SpinRight: Car_spinright(speed1, speed2); break;
         }
-    }    
-        
-   
-    
+    }
+
+	//===========================================================
+	//	走行モード４＝車の動き
+	//===========================================================
+    //% blockId=Tinybit_motor_pid block="Tinybit_motor_pid|motor_left:%sp_L|motor_right:%sp_R"
+    //% color="#006400"
+    //% weight=90
+    //% sp_L.min=-255 sp_L.max=255  sp_R.min=-255 sp_R.max=255
+    //% blockGap=10
+    //% name.fieldEditor="gridpicker" name.fieldOptions.columns=4
+    export function car_sport(sp_L:number,sp_R:number)
+    {
+        let buf = pins.createBuffer(5);
+        buf[0] = MOTOR;
+
+        if (sp_L < 0)	//逆回転
+        {
+            buf[1] = 0;
+            buf[2] = -sp_L;
+        }
+        else			//正回転
+        {
+            buf[1] = sp_L;
+            buf[2] = 0;
+        }
+
+        if (sp_R < 0)	//逆回転
+        {
+            buf[3] = 0;
+            buf[4] = -sp_R;
+        }
+        else			//正回転
+        {
+            buf[3] = sp_R;
+            buf[4] = 0;
+        }
+
+        if(sp_L>=0 && sp_R>=0)		//正、左、右
+        {
+            car_flag_new = 0;
+        }
+        else if(sp_L<0 && sp_R<0)	//反
+        {
+            car_flag_new = 1;
+        }
+        else if(sp_L>0 && sp_R<0)	//左旋回
+        {
+            car_flag_new = 2;
+        }
+        else if(sp_L<0 && sp_R>0)	//右旋回
+        {
+            car_flag_new = 3;
+        }
+
+        if(car_flag_new != car_flag_old)		//状態の変更あり
+        {
+            let bufff = pins.createBuffer(5);
+            bufff[0] = MOTOR;
+            bufff[1] = 0; bufff[2] = 0; bufff[3] = 0; bufff[4] = 0;
+            pins.i2cWriteBuffer(PWM_ADD, buf);	//停止100ms
+            basic.pause(100);
+            car_flag_old = car_flag_new;
+        }
+
+        pins.i2cWriteBuffer(PWM_ADD, buf);
+    }
+
+	//===========================================================
+	//	ラインセンサー
+	//===========================================================
     //% blockId=Tinybit_Line_Sensor block="Line_Sensor|direct %direct|value %value"
     //% weight=89
     //% blockGap=10
     //% color="#006400"
     //% name.fieldEditor="gridpicker" name.fieldOptions.columns=12
     export function Line_Sensor(direct: enPos, value: enLineState): boolean {
-
         let temp: boolean = false;
         pins.setPull(DigitalPin.P13, PinPullMode.PullNone);
         pins.setPull(DigitalPin.P14, PinPullMode.PullNone);
         switch (direct) {
             case enPos.LeftState: {
-                if (pins.digitalReadPin(DigitalPin.P13) == value) {              
-                    temp = true;                  
+                if (pins.digitalReadPin(DigitalPin.P13) == value) {
+                    temp = true;
                 }
-                else {                  
-                     temp = false;
+                else {
+                    temp = false;
                 }
                 break;
             }
 
             case enPos.RightState: {
-                if (pins.digitalReadPin(DigitalPin.P14) == value) {              
-                    temp = true;                  
+                if (pins.digitalReadPin(DigitalPin.P14) == value) {
+                    temp = true;
                 }
                 else {
                     temp = false;
@@ -375,49 +443,54 @@ namespace Tinybit {
             }
         }
         return temp;
-
     }
 
+	//===========================================================
+	//	音量センサー
+	//===========================================================
     //% blockId=Tinybit_Voice_Sensor block="Voice Sensor return"
     //% weight=88
     //% blockGap=10
     //% name.fieldEditor="gridpicker" name.fieldOptions.columns=12
     export function Voice_Sensor(): number {
 	    //pins.setPull(DigitalPin.P1, PinPullMode.PullUp);
-        let temp  = 0;		
-        temp = pins.analogReadPin(AnalogPin.P1);           
-            
+        let temp  = 0;
+        temp = pins.analogReadPin(AnalogPin.P1);
         return temp;
-
     }
-        
+
+	//===========================================================
+	//	超音波センサー（５回測距して、中央３値の平均）
+	//===========================================================
     //% blockId=Tinybit_Ultrasonic_Car block="ultrasonic return distance(cm)"
     //% color="#006400"
     //% weight=87
     //% blockGap=10
     //% name.fieldEditor="gridpicker" name.fieldOptions.columns=4
     export function Ultrasonic_Car(): number {
-
        	let list:Array<number> = [0, 0, 0, 0, 0];
-				for (let i = 0; i < 5; i++)
-				{
-					pins.setPull(DigitalPin.P16, PinPullMode.PullNone);
-					pins.digitalWritePin(DigitalPin.P16, 0);
-					control.waitMicros(2);
-					pins.digitalWritePin(DigitalPin.P16, 1);
-					control.waitMicros(15);
-					pins.digitalWritePin(DigitalPin.P16, 0);
-					let d = pins.pulseIn(DigitalPin.P15, PulseValue.High, 43200);
-					list[i] = Math.floor(d / 40);
-				}
-				list.sort();
-				let length = (list[1] + list[2] + list[3])/3;
-				return  Math.floor(length);
+		for (let i = 0; i < 5; i++)
+		{
+			pins.setPull(DigitalPin.P16, PinPullMode.PullNone);
+			pins.digitalWritePin(DigitalPin.P16, 0);
+			control.waitMicros(2);
+			pins.digitalWritePin(DigitalPin.P16, 1);
+			control.waitMicros(15);
+			pins.digitalWritePin(DigitalPin.P16, 0);
+			let d = pins.pulseIn(DigitalPin.P15, PulseValue.High, 43200);
+			list[i] = Math.floor(d / 40);
+		}
+		list.sort();
+		let length = (list[1] + list[2] + list[3])/3;
+		return  Math.floor(length);
     }
-        
+
+	//===========================================================
+	//	超音波センサー Ver2（１度の測距）
+	//===========================================================
     //% blockId=Tinybit_Ultrasonic_CarV2 block="ultrasonic for V2 return distance(cm)"
     //% color="#006400"
-    //% weight=87
+    //% weight=86
     //% blockGap=10
     //% name.fieldEditor="gridpicker" name.fieldOptions.columns=4
     export function Ultrasonic_CarV2(): number {
@@ -427,76 +500,7 @@ namespace Tinybit {
 		pins.digitalWritePin(DigitalPin.P16, 1);
 		control.waitMicros(10);
 		pins.digitalWritePin(DigitalPin.P16, 0);
-
 		let d = pins.pulseIn(DigitalPin.P15, PulseValue.High, 500 * 58);
         return  Math.floor(d / 58);
-
     }
-
-    //% blockId=Tinybit_motor_pid block="Tinybit_motor_pid|motor_left:%sp_L|motor_right:%sp_R"
-    //% color="#006400"
-    //% weight=87
-    //% sp_L.min=-255 sp_L.max=255  sp_R.min=-255 sp_R.max=255
-    //% blockGap=10
-    //% name.fieldEditor="gridpicker" name.fieldOptions.columns=4
-    export function car_sport(sp_L:number,sp_R:number)
-    {
-        let buf = pins.createBuffer(5);
-        buf[0] = MOTOR;
-
-        if (sp_L < 0)//反转
-        {
-            buf[1] = 0;
-            buf[2] = -sp_L;
-        }
-        else //正转
-        {
-            buf[1] = sp_L;
-            buf[2] = 0;
-        }
-
-        if (sp_R < 0)//反转
-        {
-            buf[3] = 0;
-            buf[4] = -sp_R;
-        }
-        else //正转
-        {
-            buf[3] = sp_R;
-            buf[4] = 0;
-        }
-
-        if(sp_L>=0 && sp_R>=0) //正、左、右
-        {
-            car_flag_new = 0;
-        }
-        else if(sp_L<0 && sp_R<0)//反
-        {
-            car_flag_new = 1;
-        }
-        else if(sp_L>0 && sp_R<0)//左旋
-        {
-            car_flag_new = 2;
-        }
-        else if(sp_L<0 && sp_R>0)//右旋
-        {
-            car_flag_new = 3;
-        }
-
-
-        if(car_flag_new != car_flag_old) //状态发生变化
-        {
-            let bufff = pins.createBuffer(5);
-            bufff[0] = MOTOR;
-            bufff[1] = 0; bufff[2] = 0; bufff[3] = 0; bufff[4] = 0;
-            pins.i2cWriteBuffer(PWM_ADD, buf);//停止100ms
-            basic.pause(100);
-            car_flag_old = car_flag_new;
-        }
-
-        pins.i2cWriteBuffer(PWM_ADD, buf);
-
-    }
-
-
 }
